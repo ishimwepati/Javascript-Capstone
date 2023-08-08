08b0dfec51c16f265aafc7ff30b63f3b1da051f8
ishimwepati <wazacode@gmail.com>
2023-08-08 14:12:15 +0300

    Closing Icon added

--- src/modules/displayMeals.js
+++ src/modules/displayMeals.js
@@ -1,4 +1,4 @@
-import getMealDetails from './getMealDetails'; // Import your function to fetch meal details
+import getMealDetails from './getMealDetails'; 
 
 const showMeals = (data) => {
     const itemBox = document.getElementById('items-box');
@@ -18,57 +18,49 @@ const showMeals = (data) => {
         const heartIcon = document.createElement('i');
         heartIcon.classList.add('far', 'fa-heart', 'heart-icon');
 
-        const commentButton = document.createElement('button'); // Creating the See More button
+        const commentButton = document.createElement('button'); 
         commentButton.classList.add('comment-button');
-        commentButton.textContent = 'See More';
-        commentButton.style.display = 'block'; // Set the display property
-
+        commentButton.textContent = 'Comment';
+        commentButton.style.display = 'block'; 
         itemContainer.appendChild(itemImg); 
         itemContainer.appendChild(desc);
         itemContainer.appendChild(heartIcon);
-        itemContainer.appendChild(commentButton); // Appending the See More button
+        itemContainer.appendChild(commentButton); 
 
-        // Add click event listener to the See More button
+       
         commentButton.addEventListener('click', async () => {
-            const mealDetails = await getMealDetails(item.idMeal); // Fetch full meal details
-            openDetailsPopup(mealDetails); // Open the details popup
+            const mealDetails = await getMealDetails(item.idMeal); 
+            openDetailsPopup(mealDetails); 
         });
 
         itemBox.appendChild(itemContainer);
     });
 };
 
-// Open the details popup and display meal details
 const openDetailsPopup = (mealDetails) => {
-    // Create and style a modal/pop-up element
-    const popup = document.createElement('div');
-    popup.classList.add('popup'); // Style this class using CSS
 
-    // Create a close button element
-    const closeButton = document.createElement('button');
-    closeButton.classList.add('popup-close');
-    closeButton.textContent = 'X';
+    const popup = document.createElement('div');
+    popup.classList.add('popup'); 
+
+
+    const closeButton = document.createElement('i');
+    closeButton.classList.add('popup-close', 'fas', 'fa-times'); 
     
-    // Add event listener to close the popup when the close button is clicked
     closeButton.addEventListener('click', () => {
         document.body.removeChild(popup);
     });
 
-    // Create content for the popup (meal details)
+
     const mealInfo = document.createElement('div');
     mealInfo.innerHTML = `
         <h2>${mealDetails.strMeal}</h2>
         <img src="${mealDetails.strMealThumb}" alt="${mealDetails.strMeal}" class="popup-image">
         <p>${mealDetails.strInstructions}</p>
     `;
-   
 
-    // Append content to the popup
-    popup.appendChild(closeButton); // Add the close button
+    popup.appendChild(closeButton); 
     popup.appendChild(mealInfo);
-  
 
-    // Append the popup to the DOM
     document.body.appendChild(popup);
 };
 
