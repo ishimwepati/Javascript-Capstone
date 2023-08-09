4e4f5e7b78aca1b6e62050820ff7963576409eff
Xghayor <cc.baqir@gmail.com>
2023-08-09 12:17:29 +0100

    Add homepage item counter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import './style.css';
 import './comment.css';
 import showMeals from './modules/displayMeals';
 import getMeals from './modules/getMeals';
+import homeCounter from './modules/homeCounter';
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const mealsData = await getMeals();
+        const mealsLength = mealsData.meals.length
         showMeals(mealsData);
+        homeCounter(mealsLength);
     } catch (error) {
         console.error('Error getting meals:', error);
     }
