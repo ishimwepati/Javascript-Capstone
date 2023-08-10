bed1241331e5e03bf23ffd0a07d287d5b6741384
Xghayor <cc.baqir@gmail.com>
2023-08-10 19:51:27 +0100

    Update test according to required changes

--- src/modules/tests/commentsCounter.test.js
+++ src/modules/tests/commentsCounter.test.js
@@ -12,11 +12,17 @@ describe('commentsCounter', () => {
   it('updates the comment counter text content', () => {
     const commentCount = 5;
 
-    // calling counter function
     commentsCounter(commentCount);
 
     const commentCounterElement = document.querySelector('.comment-counter');
     expect(commentCounterElement.textContent).toBe(`Comments: ${commentCount}`);
+  });
+
+  it('handles 0 items on the page', () => {
+    commentsCounter(0);
+
+    const commentCounterElement = document.querySelector('.comment-counter');
+    expect(commentCounterElement.textContent).toBe('Comments: 0');
   });
 
   it('does not throw error when commentCounter is not found', () => {
